8cbc139cbb23523c09400a0474b186c7b237932d
riopond65 <160642871+riopond@users.noreply.github.com>
2026-08-18 11:35:58 -0400

    completed solving for problem one, result will output to user

--- problem_one.py
+++ problem_one.py
@@ -1,7 +1,23 @@
-print("Hello new Python Program!")
+# Problem 1:
+#   If we list all the numbers below 10 that are multiples of 3 or 5, 
+#   we get 3, 5, 6, and 9. The sum of these multiples is 23.
+# 
+#   Find the sum of all the multiples of 3 or 5 below 1000.
 
-# Integer variable that will hold the sum of the multiples
-sum = 0
-# Constants relevant to this problem
-THREE = 3
-FIVE = 5
+
+# Lists of multiples relevant to this problem
+threes = list(range(3, 1000, 3))
+fives = list(range(5, 1000, 5))
+
+# Loop through the lists and remove duplicate multiples from the fives list
+for n in threes:
+    for m in fives:
+        if n == m:
+            fives.remove(m)
+
+# Using the sum() method, add all elements from the two lists
+multiplesSum = sum(threes) + sum(fives)
+
+# Output the result to the user
+print("The sum of all multiples of 3 and/or 5 are: ")
+print(multiplesSum)
