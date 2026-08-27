0d3c1cdb7604e80d4d63cf8a20684350500e8139
riopond65 <160642871+riopond@users.noreply.github.com>
2026-08-27 11:46:58 -0400

    initialised the file for problem 3 and started working on a solution- currently an inefficient calculation of factors is in place.

---
+++ problem_003.py
@@ -0,0 +1,41 @@
+# Problem 3:
+# The prime factors of 13195 are 5, 7, 13, and 29.
+# What is the largest prime factor of the number 600851475143?
+
+
+def primeFinder(number):
+    """ A basic function for determining whether an integer is 
+    a prime, returning True for prime values and False otherwise """
+    # First, make sure the factor is NOT an even number or 2
+    if (number % 2 == 0) and (number != 2):
+        return False
+
+    # Loop that checks each odd number that can possibly be a factor
+    # If the number divides by i, the number is NOT prime
+    for i in range(3, (number / 2) + 1, 2):
+        if (number % i) == 0:
+            return False
+
+    # If all previous checks fail, then the number has no factors
+    # Therefore, it is prime
+    return True
+
+# Store the given number
+# This makes it easy to adjust for different scenarios
+given = 600851475143
+half = int(given / 2) + 1
+
+# Initialise a number to store the greatest prime factor
+greatestPrime = None
+
+# Start a loop to check for all possible factors
+for i in range(half):
+    current = half - i
+    if (given % current) == 0:
+        if(primeFinder(current)):
+            greatestPrime = current
+            break
+
+if greatestPrime != None:
+    print(f"The greatest prime factor of the given number {given} is: ")
+    print(greatestPrime)
