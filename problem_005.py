2173bad5dda71ea9603d62ea59778242c9ab5eb4
riopond65 <160642871+riopond@users.noreply.github.com>
2026-09-17 11:26:01 -0400

    Re-worked Problem 5 to use a different, much more efficient approach by calculating the Least Common Multiple. Utilised the analyser file's primeFinder module.

--- problem_005.py
+++ problem_005.py
@@ -6,17 +6,37 @@
 #   by all of the numbers from 1 to 20?
 
 
-factor = 19
-found = False
+from analyser import primeFinder
 
-while not found:
-    multiple = 20 * factor
+# The dictionary will store the greatest power of each prime number
+prime_factorisation = {}
+multiple = 1
 
-    for i in range(3, 19):
-        if (multiple % i) != 0:
-            factor += 1
-            break
-        elif i == 19:
-            found = True
+for i in range(2, 21):
+    # The prime will first be stored as a dictionary key
+    if primeFinder(i) == True:
+        prime_factorisation[i] = 1
+    # No prime greater than 3 will exceed power of 1 for range 1-20
+    elif i % 3 == 0:
+        power = 1
+        quotient = i / 3
+        # Keep looping until the final root is found
+        while quotient % 3 == 0:
+            power += 1
+            quotient = quotient / 3
+        if prime_factorisation[3] < power:
+            prime_factorisation[3] = power
+    elif i % 2 == 0:
+        power = 1
+        quotient = i / 2
+        while quotient % 2 == 0:
+            power += 1
+            quotient = quotient / 2
+        if prime_factorisation[2] < power:
+            prime_factorisation[2] = power
+
+for k, v in prime_factorisation.items():
+    print(f"key: {k} value: {v}")
+    multiple *= pow(k, v)
 
 print(f"The result is: {multiple}")
